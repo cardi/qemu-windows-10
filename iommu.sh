dcd7ff654fd6816c96d59ae42ac4c69bc386406c
Calvin Ardi <calvin@isi.edu>
2019-07-22 20:02:29 -0700

    iommu: add script

---
+++ iommu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# get a list of devices and respective IOMMU group numbers
+# (forgot where this script came from)
+
+shopt -s nullglob
+for d in /sys/kernel/iommu_groups/*/devices/*; do
+    n=${d#*/iommu_groups/*}; n=${n%%/*}
+    printf 'IOMMU Group %s ' "$n"
+    lspci -nns "${d##*/}"
+done;
